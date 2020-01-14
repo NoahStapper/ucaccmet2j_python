fdeda36dc72b3be9714ca9de0db887fe72414797
Noah <noahstapper@yahoo.com>
2020-01-14 15:08:45 +0100

    managed to print all values from Seattle stations

--- Assignment_5.py
+++ Assignment_5.py
@@ -4,7 +4,25 @@ import json
 
 with open('precipitation.json', encoding='utf8') as file:  
     data = json.load(file)
+<<<<<<< Updated upstream
 print(data)
+=======
+
+
+#print(data[0]['station'])
+
+seattle_data = 'GHCND:US1WAKG0038'
+
+for measurement in data:
+    if (measurement['station']) == seattle_data:
+        print(measurement['date']['value'])
+
+    
+    
+
+#print(f'{station}:{(data[0]['GHCND:US1WAKG0038'])}')
+
+>>>>>>> Stashed changes
 
 
 
