3c02d01217eb6d9d0b4d08a6dac39c23856577d2
Noah <noahstapper@yahoo.com>
2020-01-14 17:10:37 +0100

    total values per month

--- Assignment_5.py
+++ Assignment_5.py
@@ -4,23 +4,40 @@ import json
 
 with open('precipitation.json', encoding='utf8') as file:  
     data = json.load(file)
-print(data)
 
 
 #print(data[0]['station'])
 
 seattle_data = 'GHCND:US1WAKG0038'
 
-result = []
+value_seattle = []
+date_seattle = []
 for measurement in data:
     if (measurement['station']) == seattle_data:
-        result.append((measurement['value']))
-        result.append((measurement['date']))
-print(result)
+        value_seattle.append((measurement['value']))
+        date_seattle.append((measurement['date'])[5:7])
+        
+# weather_dic = dict(zip(date_seattle, value_seattle))
+# print(weather_dic)
 
 
 
 
+weather_dic = {}
+i = 0
+for month in date_seattle:
+    if month in weather_dic:
+        weather_dic[month] += value_seattle[i]
+    else:
+        weather_dic[month] = value_seattle[i]
+    i += 1
+print(weather_dic)
+
+
+
+        
+
+        
 
 
 
